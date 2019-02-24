73e00e7d92f162ef781c11b86dfb3bedf8d9e2b6
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-02-23 22:56:13 -0800

    Create set2.6.c

---
+++ set2.6.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,a[100],count=0,i,j;
+clrscr();
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+    scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+    count=0;
+    for(j=i+1;j<n;j++)
+    {
+        if(a[i]==a[j])
+        {
+            count++;
+            a[j]='&';
+        }
+    }
+    if(count==0)
+    {
+        printf("%d",a[i]);
+        break;
+    }
+}
+getch();
+}
