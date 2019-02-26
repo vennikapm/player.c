ed73c2c4f10e58690ed742d937841a6dbe9e59c5
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-02-26 08:24:53 -0800

    Create set4.8.c

---
+++ set4.8.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,i;
+    clrscr();
+    scanf("%d",&n);
+    for(i=2;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            if(i%2==0)
+            {
+                printf("%d ",i);
+            }
+        }
+        
+    }
+    getch();
+}
