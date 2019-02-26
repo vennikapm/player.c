423468dd10e52ee317f9ffff3a9aa8e914ef4366
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-02-26 09:29:42 -0800

    Create set5.9.c

---
+++ set5.9.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n;
+    clrscr();
+    scanf("%d",&n);
+    if(n>=1&&n<=100000)
+    {
+    if(n>=-32,768&&n<=32,767)
+    {
+        printf("int");
+    }
+    else
+    {
+    printf("long");
+    }
+    }
+    getch();
+}
