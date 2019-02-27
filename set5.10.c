2c36ecac96871eb1b06a9af8b6569a98a23662d7
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-02-27 12:45:09 +0530

    Update set5.10.c

--- set5.10.c
+++ set5.10.c
@@ -2,18 +2,25 @@
 #include<conio.h>
 void main()
 {
-    int n,i;
+    int n,i,flag=0;
     clrscr();
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=2;i<n;i++)
     {
         if(n%i==0)
         {
-            if(i%2!=0)
-            {
-                printf("%d ",i);
+            
+                flag++;
             }
-        }
+        
+    }
+    if(flag>0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
     }
     getch();
 }
