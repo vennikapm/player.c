360d68d8aa56fb93e3164ec1154a198af2ae234c
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-02-27 15:18:23 +0530

    Create set5.2.c

---
+++ set5.2.c
@@ -0,0 +1,29 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,a[100],i,j=0;
+    clrscr();
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<a[i+1])
+        {
+            j++;
+        }
+    }
+    if(n-1==j)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+getch();
+}
