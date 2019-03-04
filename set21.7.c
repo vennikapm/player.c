b5baf4979de79f9bf7d39d300bacd85bb9455207
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-03-04 07:48:10 -0800

    Create set21.7.c

---
+++ set21.7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+    int a,b,n;
+    clrscr();
+    scanf("%d %d",&a,&b);
+    n=(a*b)/2;
+    printf("%d",n);
+    getch();
+}
