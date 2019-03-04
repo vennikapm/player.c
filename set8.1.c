2f2f0bb9e679388fbbeea15240232e0c7a754da1
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-03-04 06:46:50 -0800

    Create set8.1.c

---
+++ set8.1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,a[100],i;
+    clrscr();
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        if(a[i]>a[i+1])
+        {
+            printf("%d ",a[i]);
+        }
+        else
+        {
+            printf("%d ",a[i+1]);
+        }
+    }
+    getch();
+}
