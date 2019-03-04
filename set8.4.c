954aaae2359dbd2f5b5b5db00439a62e11734a5b
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-03-04 07:22:07 -0800

    Create set8.4.c

---
+++ set8.4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+    int a,b,n;
+    clrscr();
+    scanf("%d %d",&a,&b);
+    n=pow(a,b);
+    printf("%d",n);
+    getch();
+}
