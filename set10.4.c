ff8a6291885c1f00c988170412f85334a9ffd56f
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-03-05 06:51:18 -0800

    Create set10.4.c

---
+++ set10.4.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a[100],i,j,n,count=0;
+    clrscr();
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                count++;
+            }
+        }
+    }
+    if(count>0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    getch();
+}
