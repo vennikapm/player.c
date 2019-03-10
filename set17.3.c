966f03f4d11f7507f94e839d5fe5c0b661d9bf9e
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-03-09 22:38:20 -0800

    Create set17.3.c

---
+++ set17.3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[100],i,n,f=0,k;
+clrscr();
+scanf("%d %d",&n,&k);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+if(a[i]==k)
+{
+printf("yes");
+break;
+}
+else
+{
+f++;
+}
+}
+if(f==n)
+{
+printf("no");
+}
+getch();
+}
