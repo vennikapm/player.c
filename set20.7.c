1cbef5a2bec53d98da919780ca368e9260770200
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-03-10 07:33:43 -0700

    Create set20.7.c

---
+++ set20.7.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a[100],i,j,ans,n,max=0,min=0;
+    clrscr();
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    max=a[0];
+    min=a[0];
+    for(i=0;i<n;i++)
+    {
+     if(a[i]>max)
+     {
+         max=a[i];
+     }
+     if(a[i]<min)
+     {
+         min=a[i];
+     }
+    }
+    ans=max-min;
+    printf("%d",ans);
+
+getch();
+}
