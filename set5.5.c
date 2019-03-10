7a4f26b4f2872f26790349a15dd8422d99c65afa
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-03-10 07:09:38 -0700

    Create set5.5.c

---
+++ set5.5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void main()
+{
+    int p,a,i,j,perimeter,area,flag=0;
+	scanf("%d %d",&p,&a);
+	for(i=1;i<p;i++)
+	{
+	    for(j=1;j<p;j++)
+	{
+	    perimeter=2*(i+j);
+	    area=i*j;
+	    if(perimeter==p && area==a)
+	    {
+	        printf("yes");
+	        flag=1;
+	        break;
+	    }
+	}
+	if(flag==1)
+	{
+	    break;
+	}
+	}
+	if(flag==0)
+	{
+	    printf("no");
+	}
+	getch();
+}
