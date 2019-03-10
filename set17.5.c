112be4560084270e537114b14d1a238f91b71ac5
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-03-09 22:56:47 -0800

    Create set17.5.c

---
+++ set17.5.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a[100],i,n,k,f=0,c;
+    clrscr();
+    
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    c=a[0];
+    for(i=0;i<n;i++)
+    {
+    
+         if((a[i]>c)&&(a[i]>k))
+         {
+             printf("%d",a[i]);
+             break;
+         }
+         else
+         {
+             c=a[i];
+         }
+    }
+    
+        
+    getch();
+}
