4c31c047d7f006477621aa6cef70b85520f2b406
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-03-14 10:09:30 -0700

    Create set12.4.c

---
+++ set12.4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<conio.h>
+#include<math.h>
+void main() 
+{
+	int a,b,c,res,ans;
+	scanf("%d %d %d",&a,&b,&c);
+	res=pow(a,b);
+	ans=res/c;
+	printf("%d",ans);
+	getch();
+}
