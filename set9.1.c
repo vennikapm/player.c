aecceb6689f66e5c6aa6b3d42422417324ecd045
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-03-14 08:30:16 -0700

    Create set9.1.c

---
+++ set9.1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+void main() 
+{
+	int n,a[100],i,count=1,max=0,res,j;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	
+	}
+	for(i=0;i<n;i++)
+	{
+     for(j=i+1;j<n;j++)
+     {
+         res=(a[i])&(a[j]);
+       if(res>max)
+       {
+           max=res;
+       }
+     }
+	}
+	if(n==1)
+	{
+	    printf("%d",a[0]);
+	}
+	else
+	{
+	printf("%d",max);
+	}
+	getch();
+}
