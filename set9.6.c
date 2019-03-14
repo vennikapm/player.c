1a31589b1aec7d787b5a9dcfbe3f26c6bbc2a887
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-03-14 08:41:16 -0700

    Create set9.6.c

---
+++ set9.6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main() 
+{
+	int n,a[100],i,count=1,max=0,res=0,j;
+	scanf("%d",&n);
+	
+     for(i=0;i<n;i++)
+     {
+         scanf("%d",&a[i]);
+     }
+     for(i=0;i<n;i++)
+     {
+         res=res^a[i];
+     }
+     printf("%d",res);
+     getch();
+}
