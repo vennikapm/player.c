a28843002159e71bdb388c5882cb57578d4f4680
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-03-16 09:04:42 -0700

    Create set11.5.c

---
+++ set11.5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<conio.h>
+void main() 
+{
+	int a[10],n,sum=0,i,j,temp;
+	scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+   for(i=0;i<n;i++)
+   {
+       for(j=i+1;j<n;j++)
+       {
+           if(a[i]>a[j])
+           {
+               temp=a[i];
+               a[i]=a[j];
+               a[j]=temp;
+           }
+       }
+        printf("%d ",i+1);
+   }
+  	getch();
+}
