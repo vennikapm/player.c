538aab297d7b18b4916c1046efe07efef5fd7f9b
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-03-22 07:10:03 -0700

    Create set16.10.c

---
+++ set16.10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void main()
+{
+    int n,m,bin=0,rem,count=0,rem2,power=1,mul;
+    scanf("%d %d",&n,&m);
+    mul=n|m;
+    while(mul>0)
+    {
+        rem=mul%2;
+        bin=bin+rem*power;
+        mul=mul/2;
+        power=power*10;
+    }
+    
+    while(bin>0)
+    {
+        rem2=bin%10;
+        if(rem2==1)
+        {
+            count++;
+        }
+        bin=bin/10;
+    }
+    printf("%d",count);
+}
