982dd3e9ef47a0597f1a454b634caee126bb5362
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-03-24 06:28:59 -0700

    Update set5.4.c

--- set5.4.c
+++ set5.4.c
@@ -1,21 +1,23 @@
-#include<stdio.h>
-#include<conio.h>
-void main()
+#include <stdio.h>
+#include<string.h>
+int main()
 {
-    char a[100],b[100];
-    int i,j=0,k,n,count=0;
-    scanf("%s %d",a,&n);
-    for(i=n;a[i]!='\0';i++)
-    {
-        b[j]=a[i];
-        j++;
-    }
-    for(i=0;i<n;i++)
-    {
-        b[j]=a[i];
-        j++;
-    }
-    b[j]='\0';
-    printf("%s",b);
-    getch();
+	int n,k,i,j;
+	char a[10],temp;
+	scanf("%s",a);
+	n=strlen(a);
+	scanf("%d",&k);
+	for(i=0;i<k;i++)
+	{
+	    temp=a[n-1];
+	    {
+        for(j=n-2;j>=0;j--)
+ 	{
+	   a[j+1]=a[j];
+	}
+	    a[0]=temp;
+	   
+	} 
+	printf("%s",a);
+	return 0;
 }
